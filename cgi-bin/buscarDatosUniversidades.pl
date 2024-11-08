3178548b325224c8cb8e1391ed01c8ac73e6cbdf
SrEstrada <sestradaa@unsa.edu.pe>
2024-11-08 10:58:02 -0500

    Creación de la estructura inicial del archivo Perl para el manejo de la búsqueda

---
+++ cgi-bin/buscarDatosUniversidades.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI ':standard';
+
+print header(-charset => 'UTF-8');
+print start_html(-title=>'Resultado de Búsqueda');
+
+# Estructura base para leer datos en siguientes commits
+print end_html;
