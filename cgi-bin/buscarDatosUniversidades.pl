39b0248c5168157d2beb75eb96be1ff05e96024a
SrEstrada <sestradaa@unsa.edu.pe>
2024-11-09 03:27:39 -0500

    Los resultados de la búsqueda ahora se muestran con etiquetas de párrafo y un encabezado que indica el inicio de los resultados.

--- cgi-bin/buscarDatosUniversidades.pl
+++ cgi-bin/buscarDatosUniversidades.pl
@@ -12,11 +12,25 @@ if (!$nombre) {
     print "<p>Error: Debe ingresar un nombre de universidad.</p>";
 } else {
     open my $fh, '<', 'Data_Universidades_Lab06.csv' or die "No se puede abrir el archivo: $!";
+    
+    my $found = 0;  # Variable para indicar si encontramos una coincidencia
     while (my $line = <$fh>) {
         chomp $line;
-        # El procesamiento de datos se realizará en siguientes commits
+        
+        # Si el nombre buscado se encuentra en la línea actual
+        if ($line =~ /\Q$nombre\E/i) {
+            print "<h3>Resultado de la búsqueda:</h3>";
+            print "<p>Universidad encontrada: $line</p>";
+            $found = 1;
+            last;  # Salir del ciclo una vez encontrada la coincidencia
+        }
     }
+    
+    # Mostrar mensaje si no se encontraron coincidencias
+    print "<p>No se encontraron resultados para \"$nombre\".</p>" unless $found;
+
     close $fh;
 }
 
 print end_html;
+
