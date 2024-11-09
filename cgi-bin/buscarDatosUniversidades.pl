e9049357f1068607a26ef58c575ba663f635c1e9
SrEstrada <sestradaa@unsa.edu.pe>
2024-11-09 02:45:23 -0500

    El css, excelente, pero al quere adelantar el trabajo de manera rápida mi pl, digamos que no funciono, en este commit rehago la lógica de mi pl de manera más lenta, en este commit se establece la conexión y lectura básica del archivo Data_Universidades_Lab06.csv

--- cgi-bin/buscarDatosUniversidades.pl
+++ cgi-bin/buscarDatosUniversidades.pl
@@ -1,44 +1,22 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use CGI;
-use Text::CSV;
+use CGI ':standard';
 
-# Configuración inicial CGI y encabezado HTML
-my $cgi = CGI->new;
-print $cgi->header(-charset => 'UTF-8');
-print $cgi->start_html(-title => 'Resultado de Búsqueda');
+print header(-charset => 'UTF-8');
+print start_html(-title=>'Resultado de Búsqueda');
 
-# Parámetro de búsqueda ingresado en el formulario HTML
-my $nombre_universidad = $cgi->param('nombre') || '';
+my $nombre = param('nombre');
 
-# Configuración de archivo CSV y lectura de datos
-my $archivo_csv = "../cgi-bin/Data_Universidades_Lab06.csv";
-my $csv = Text::CSV->new({ binary => 1 });
-
-open my $fh, "<:encoding(utf8)", $archivo_csv or die "No se pudo abrir '$archivo_csv' $!\n";
-
-# Búsqueda de coincidencias en el archivo CSV
-my @resultados;
-while (my $row = $csv->getline($fh)) {
-    my ($nombre, $ubicacion, $tipo) = @$row;
-    if ($nombre =~ /\Q$nombre_universidad\E/i) {
-        push @resultados, { nombre => $nombre, ubicacion => $ubicacion, tipo => $tipo };
-    }
-}
-close $fh;
-
-# Generación de respuesta HTML basada en los resultados encontrados
-print "<html><head><title>Resultados</title></head><body>";
-print "<h1>Resultados de búsqueda</h1>";
-
-if (@resultados) {
-    print "<ul>";
-    foreach my $uni (@resultados) {
-        print "<li>$uni->{nombre} - $uni->{ubicacion} ($uni->{tipo})</li>";
-    }
-    print "</ul>";
+if (!$nombre) {
+    print "<p>Error: Debe ingresar un nombre de universidad.</p>";
 } else {
-    print "<p>No se encontraron resultados para '$nombre_universidad'.</p>";
+    open my $fh, '<', 'Data_Universidades_Lab06.csv' or die "No se puede abrir el archivo: $!";
+    while (my $line = <$fh>) {
+        chomp $line;
+        # El procesamiento de datos se realizará en siguientes commits
+    }
+    close $fh;
 }
-print "</body></html>";
+
+print end_html;
