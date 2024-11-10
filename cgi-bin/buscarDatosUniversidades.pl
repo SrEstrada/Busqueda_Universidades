1b785c48bbfa9b280ab6abdcadae69a79b0127ff
SrEstrada <sestradaa@unsa.edu.pe>
2024-11-10 03:00:05 -0500

    Agregué una subrutina realizarBusqueda() para manejar la búsqueda con los 4 filtros (nombre, periodo, departamento y denominación) y generarHTML() para generar el HTML con los resultados, ordenando y modularizando el código.

--- cgi-bin/buscarDatosUniversidades.pl
+++ cgi-bin/buscarDatosUniversidades.pl
@@ -3,34 +3,54 @@ use strict;
 use warnings;
 use CGI ':standard';
 
-print header(-charset => 'UTF-8');
-print start_html(-title=>'Resultado de Búsqueda');
-
+# Captura de parámetros
 my $nombre = param('nombre');
+my $periodo = param('periodo');
+my $departamento = param('departamento');
+my $programa = param('programa');
 
-if (!$nombre) {
-    print "<p>Error: Debe ingresar un nombre de universidad.</p>";
-} else {
-    open my $fh, '<', 'Data_Universidades_Lab06.csv' or die "No se puede abrir el archivo: $!";
+# Subrutina principal que realiza la búsqueda
+sub realizarBusqueda {
+    my ($nombre, $periodo, $departamento, $programa) = @_;
+    my @resultados;
     
-    my $found = 0;  # Variable para indicar si encontramos una coincidencia
+    open my $fh, '<', 'Data_Universidades_Lab06.csv' or die "No se puede abrir el archivo: $!";
     while (my $line = <$fh>) {
         chomp $line;
         
-        # Si el nombre buscado se encuentra en la línea actual
-        if ($line =~ /\Q$nombre\E/i) {
-            print "<h3>Resultado de la búsqueda:</h3>";
-            print "<p>Universidad encontrada: $line</p>";
-            $found = 1;
-            last;  # Salir del ciclo una vez encontrada la coincidencia
+        # Validación de coincidencias en cada criterio
+        if (($nombre eq '' or $line =~ /\Q$nombre\E/i) &&
+            ($periodo eq '' or $line =~ /\Q$periodo\E/i) &&
+            ($departamento eq '' or $line =~ /\Q$departamento\E/i) &&
+            ($programa eq '' or $line =~ /\Q$programa\E/i)) {
+            push @resultados, $line;
         }
     }
-    
-    # Mostrar mensaje si no se encontraron coincidencias
-    print "<p>No se encontraron resultados para \"$nombre\".</p>" unless $found;
-
     close $fh;
+    
+    return @resultados;
 }
 
-print end_html;
+# Subrutina que genera el HTML de respuesta
+sub generarHTML {
+    my ($nombre, $periodo, $departamento, $programa, @resultados) = @_;
+    
+    print header(-charset => 'UTF-8');
+    print start_html(-title => 'Resultado de Búsqueda');
+    
+    if (!@resultados) {
+        print "<p>No se encontraron resultados para los criterios ingresados.</p>";
+    } else {
+        print "<h3>Resultados de la búsqueda:</h3><ul>";
+        foreach my $resultado (@resultados) {
+            print "<li>Universidad encontrada: $resultado</li>";
+        }
+        print "</ul>";
+    }
+    
+    print end_html;
+}
 
+# Ejecución de búsqueda y generación de respuesta HTML
+my @resultados = realizarBusqueda($nombre, $periodo, $departamento, $programa);
+generarHTML($nombre, $periodo, $departamento, $programa, @resultados);
