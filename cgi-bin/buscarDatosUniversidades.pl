444fe2be965e1516713410361272dde38ab98744
SrEstrada <sestradaa@unsa.edu.pe>
2024-11-10 23:51:32 -0500

    Actualice el pl, cambie el dockerfile, organice mejor los archivos, mejore el códig, y aún no corre

--- cgi-bin/buscarDatosUniversidades.pl
+++ cgi-bin/buscarDatosUniversidades.pl
@@ -1,85 +1,57 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use CGI ':standard';
-use utf8;
+use CGI;
+use Text::CSV;
 
-# Creación del objeto CGI
+# Crear objeto CGI
 my $q = CGI->new;
 
-# Subrutina principal que maneja la lógica de búsqueda
-sub realizarBusqueda {
-    # Captura de parámetros del formulario
-    my $nombre = $q->param('nombre') || '';
-    my $periodo = $q->param('periodo') || '';
-    my $departamento = $q->param('departamento') || '';
-    my $tipo_gestion = $q->param('tipo_gestion') || '';
+# Capturar parámetros del formulario
+my $nombre = $q->param('nombre') || '';
+my $periodo = $q->param('periodo') || '';
+my $departamento = $q->param('departamento') || '';
+my $tipo_gestion = $q->param('gestion') || '';
 
-    # Mensaje de error si no hay criterios
-    unless ($nombre || $periodo || $departamento || $tipo_gestion) {
-        return "<p>Error: Debe ingresar al menos un criterio de búsqueda.</p>";
-    }
-
-    # Abrir el archivo CSV
-    open my $fh, '<:encoding(UTF-8)', 'Data_Universidades_Lab06.csv' or die "No se puede abrir el archivo: $!";
-
-    my @resultados;
-    while (my $line = <$fh>) {
-        chomp $line;
-        
-        # Separar los campos del CSV
-        my @campos = split /,/, $line;
-
-        # Asumimos que los campos son en este orden:
-        # [0] -> Código, [1] -> Nombre, [2] -> Tipo Gestión, [3] -> Estado Licenciamiento
-        # [4] -> Fecha Inicio, [5] -> Fecha Fin, [6] -> Periodo Licenciamiento, [7] -> Departamento, etc.
-
-        # Aplicar filtros según criterios de búsqueda
-        my $coincidencia = 1;
-        
-        $coincidencia &&= ($campos[1] =~ /\Q$nombre\E/i) if $nombre;
-        $coincidencia &&= ($campos[6] =~ /\Q$periodo\E/i) if $periodo;
-        $coincidencia &&= ($campos[7] =~ /\Q$departamento\E/i) if $departamento;
-        $coincidencia &&= ($campos[2] =~ /\Q$tipo_gestion\E/i) if $tipo_gestion;
-
-        # Si cumple todos los criterios, agregar a los resultados
-        push @resultados, $line if $coincidencia;
-    }
-
-    close $fh;
-
-    # Generar HTML con resultados
-    if (@resultados) {
-        my $html = "<h3>Resultados de la búsqueda:</h3><ul>";
-        $html .= "<li>$_</li>" for @resultados;
-        $html .= "</ul>";
-        return $html;
-    } else {
-        return "<p>No se encontraron resultados para los criterios especificados.</p>";
-    }
-}
-
-# Subrutina que genera el HTML completo de respuesta
-sub generarHTML {
-    my $contenido = shift;
-
+# Verificar si al menos un campo tiene datos
+unless ($nombre || $periodo || $departamento || $tipo_gestion) {
     print $q->header('text/html; charset=UTF-8');
-    print <<"HTML";
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Resultado de Búsqueda</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div class="resultados">
-        $contenido
-    </div>
-</body>
-</html>
-HTML
+    print "<p>Error: Debe ingresar al menos un criterio de búsqueda.</p>";
+    exit;
 }
 
-# Llamar a la subrutina principal y pasar el contenido a generarHTML
-generarHTML(realizarBusqueda());
+# Especificar la ruta del archivo CSV
+my $csv_file = "/cgi-bin/Programas de Universidades.csv";
+
+# Crear el objeto CSV
+my $csv = Text::CSV->new({ binary => 1, sep_char => '|' }) or die "Error al crear CSV: " . Text::CSV->error_diag();
+
+# Abrir archivo CSV
+open my $fh, '<', $csv_file or die "No se pudo abrir el archivo '$csv_file': $!";
+
+# Imprimir encabezado HTML
+print $q->header('text/html; charset=UTF-8');
+print "<html><head><title>Resultados de Búsqueda</title></head><body>";
+print "<h1>Resultados de la Búsqueda</h1>";
+print "<table border='1'><tr><th>Nombre</th><th>Periodo</th><th>Departamento</th><th>Gestión</th></tr>";
+
+# Leer y filtrar los datos
+while (my $row = $csv->getline($fh)) {
+    my ($csv_nombre, $csv_periodo, $csv_departamento, $csv_gestion) = @$row;
+
+    # Filtrar los resultados
+    if (($nombre eq '' || $csv_nombre =~ /\Q$nombre\E/i) &&
+        ($periodo eq '' || $csv_periodo =~ /\Q$periodo\E/i) &&
+        ($departamento eq '' || $csv_departamento =~ /\Q$departamento\E/i) &&
+        ($tipo_gestion eq '' || $csv_gestion =~ /\Q$tipo_gestion\E/i)) {
+        
+        # Imprimir resultado en tabla
+        print "<tr><td>$csv_nombre</td><td>$csv_periodo</td><td>$csv_departamento</td><td>$csv_gestion</td></tr>";
+    }
+}
+
+# Cerrar tabla y HTML
+print "</table></body></html>";
+
+# Cerrar archivo CSV
+close $fh or warn "No se pudo cerrar el archivo '$csv_file': $!";
