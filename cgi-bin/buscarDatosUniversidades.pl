1f59a5f5a9f19be665ff86a89c6bffee0d9e73c3
SrEstrada <sestradaa@unsa.edu.pe>
2024-11-11 20:58:32 -0500

    Empece desde cero, pero ya no tengo tiempo, ńo sé que pasa ni que puedo cambiar, al menos sé que se redirecciona de manera correcta

--- cgi-bin/buscarDatosUniversidades.pl
+++ cgi-bin/buscarDatosUniversidades.pl
@@ -1,28 +1,38 @@
-#!"C:/xampp/perl/bin/perl.exe"
+#!/usr/bin/perl
 use strict;
 use warnings;
 use CGI;
 use Text::CSV;
 use utf8;
 
-my $cgi = new CGI;
+# Inicializa CGI
+my $cgi = CGI->new();
 
+# Imprime el encabezado de contenido HTML
+print "Content-type: text/html\n\n";
+
+# Obtiene los parámetros de entrada del formulario CGI
 my $uniName   = $cgi->param('universityName')   || qr/.*/;
 my $licPeriod = $cgi->param('licencePeriod')    || qr/.*/;
 my $depLocal  = $cgi->param('localDepartment')  || qr/.*/;
 my $pName     = $cgi->param('programName')      || qr/.*/;
 
+# Decodifica los parámetros en caso de caracteres especiales
 utf8::decode($uniName);
 utf8::decode($licPeriod);
 utf8::decode($depLocal);
 utf8::decode($pName);
 
-my $archivo = '../ProgramasdeUniversidades.csv';
+# Ruta del archivo CSV
+my $archivo = '/usr/local/apache2/cgi-bin/ProgramasdeUniversidades.csv';
 
-open my $fh, '<', $archivo or die "No soportó";
+# Abre el archivo CSV o muestra un error si no se encuentra
+open my $fh, '<', $archivo or die "No se pudo abrir el archivo CSV: $!";
 
-my $csv = Text::CSV->new({ binary => 1, sep_char => '|'});
+# Inicializa el módulo CSV con el delimitador correcto
+my $csv = Text::CSV->new({ binary => 1, sep_char => '|' });
 
+# Lee las cabeceras y construye la fila de encabezados de la tabla HTML
 my $tableKey = "";
 if (my $fila = $csv->getline($fh)) {
     my @selectedValues = map { $fila->[$_] } (1, 4, 10, 16);
@@ -31,12 +41,18 @@ if (my $fila = $csv->getline($fh)) {
     }
 }
 
+# Variables para almacenar resultados y conteo de coincidencias
 my $tableValues = "";
 my $coincidenciasTotales = 0;
 
-while(my $fila = $csv->getline($fh)){
+# Itera sobre cada fila del archivo CSV y verifica coincidencias
+while (my $fila = $csv->getline($fh)) {
     my @selectedValues = map { $fila->[$_] } (1, 4, 10, 16);
-    if($selectedValues[0] =~ /$uniName/i && $selectedValues[1] =~ /$licPeriod/i && $selectedValues[2] =~ /$depLocal/i && $selectedValues[3] =~ /$pName/i){
+    if ($selectedValues[0] =~ /$uniName/i &&
+        $selectedValues[1] =~ /$licPeriod/i &&
+        $selectedValues[2] =~ /$depLocal/i &&
+        $selectedValues[3] =~ /$pName/i) {
+        
         $tableValues .= "<tr>";
         for my $valor (@selectedValues) {
             $tableValues .= "<td>$valor</td>\n";
@@ -46,8 +62,11 @@ while(my $fila = $csv->getline($fh)){
     }
 }
 close $fh;
-$coincidenciasTotales = "Existen $coincidenciasTotales resultados de búsqueda";
 
+# Mensaje de resultado de coincidencias
+my $resultadoBusqueda = "Existen $coincidenciasTotales resultados de búsqueda";
+
+# Genera la salida HTML
 print $cgi->header('text/html');
 
 print <<HTML;
@@ -57,7 +76,7 @@ print <<HTML;
     <meta charset="UTF-8">
     <title>Resultados Búsqueda</title>
     <style>
-        *{
+        * {
             font-family: 'Open Sans', sans-serif;
             background-color: #31304D;
         }
@@ -66,31 +85,30 @@ print <<HTML;
             width: 100%;
             background-color: #161A30;
         }
-
         th, td {
             text-align: center;
             padding: 8px;
             border: solid 2px;
             background-color: #B6BBC4;
         }
-        div{
+        div {
             display: flex;
             justify-content: center;
             align-items: center;
             flex-direction: column;
             color: #e9e5cf;
         }
-        a{
+        a {
             text-decoration: none;
         }
-        a:visited{
+        a:visited {
             color: #e9e5cf;
         }
     </style>
 </head>
 <body>
     <div>
-        <h2>$coincidenciasTotales</h2>
+        <h2>$resultadoBusqueda</h2>
         <h2><a href="../../lab09/index.html">Para volver a buscar, presione aquí</a></h2>
         <h1>Resultados de la Búsqueda</h1>
     </div>
@@ -101,4 +119,3 @@ print <<HTML;
 </body>
 </html>
 HTML
-
